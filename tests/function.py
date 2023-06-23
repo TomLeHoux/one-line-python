b3b96887ef23aa0d16977f91d02e5b108810685b
TomLeHoux <tomlehoux000@gmail.com>
2023-06-22 21:16:42 -0300

    removed testing files

--- tests/function.py
+++
@@ -1,3 +0,0 @@
-# [[print(odd(number)) for odd in [lambda number: "Is even" if number % 2 == 0 else "Is odd"]] for number in [2,11,5]]
-
-[[print(odd(number)) for number in [2,11,5]] for odd in [lambda number: "Is even" if number % 2 == 0 else "Is odd"]]
